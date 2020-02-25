e6e7e66db219798c97d38be82bebe9280770544b
tylergreason <tyler.greason@protonmail.com>
2020-02-25 17:58:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your functions here
+function map(array,fun){
+    let newArray = [];
+    array.forEach(function(e){
+        newArray.push(fun(e))
+    })
+    return newArray;
+}
+
+function reduce(array, fun, startingValue){
+    // let r = (!!starting) ? starting : src[0]
+    // let i = (!!starting) ? 0 : 1
+    let total, iterator; 
+    if (startingValue){
+        total = startingValue;
+        iterator = 0; 
+    }else{
+        total = array[0];
+        iterator = 1;
+    }
+    for (iterator;iterator<array.length;iterator++){
+        total = fun(array[iterator],total);
+    }
+    // array.forEach(function(num){
+    //     total = fun(num,total);        
+    // })
+    return total;
+    
+}
